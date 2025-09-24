aa64380e8453b41678cdf16fa0fe6bdc1e679bd4
Md. Rubayet Islam <rubayetislam16@gmail.com>
2025-09-25 00:07:13 +0600

    api

--- routes/api.php
+++ routes/api.php
@@ -11,13 +11,15 @@ Route::post('login',[AuthController::class,'login']);
 Route::post('phoneCheck',[AuthController::class,'phoneCheck']);
 Route::post('forgotPassword',[AuthController::class,'forgotPassword']);
 
-Route::get('match',[ApiController::class,'matches']);
-Route::get('match/{id}',[ApiController::class,'matchbyID']);
-Route::get('prize',[ApiController::class,'prizeTools']);
-Route::get('prize/{id}',[ApiController::class,'prizebyID']);
+
 
 Route::group(['middleware' => 'auth:api'], function() {
     Route::get('profile',[AuthController::class,'profile']);
+
+    Route::get('match',[ApiController::class,'matches']);
+    Route::get('match/{id}',[ApiController::class,'matchbyID']);
+    Route::get('prize',[ApiController::class,'prizeTools']);
+    Route::get('prize/{id}',[ApiController::class,'prizebyID']);
 
     Route::post('/checkout', [PaymentController::class, 'paymentInit']);
 
