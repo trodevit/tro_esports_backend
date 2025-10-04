9dcede3bd025953bedee9de80788161f81e16baa
Md. Rubayet Islam <rubayetislam16@gmail.com>
2025-10-04 21:12:42 +0600

    some

--- resources/views/match/index.blade.php
+++ resources/views/match/index.blade.php
@@ -185,7 +185,7 @@
                     <h5 class="modal-title" id="deleteMatchLabel">Delete Match</h5>
                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
-                <form id="deleteMatchForm" method="POST" action="{{route('matches.delete')}}">
+                <form id="deleteMatchForm" method="POST" action="{{route('matches.destroy')}}">
                     @csrf
                     @method('DELETE')
                     <div class="modal-body">
